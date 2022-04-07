71b1c011294f9c649b8f3871fb1317496e1fb65c
Ro_gdre5438_bot <64570776+G-Drennan@users.noreply.github.com>
2022-04-08 09:47:26 +1000

    Add files via upload

--- Simple Part 1/main.c
+++ Simple Part 1/main.c
@@ -1,7 +1,19 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
-#include <stdio.h>   
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>    
 
+//output var
+char *string = "Interrupts Activate!!!\r\n";
+char *current_character = 0x00; 
+
+//intput var
+#define ARRSAPCE 64
+char inputArray[ARRSAPCE];
+char currentChar = ' ';   
+int arrCounter = 0; 
+ 
 // NOTE: these are stored as pointers because they 
 //       are const values so we can't store them directly
 //       in the struct 
@@ -12,15 +24,7 @@ typedef struct SerialPort {
   byte *ControlRegister2;
   byte *DataRegister;
   byte *StatusRegister;
-} SerialPort; 
-//output var
-char *string = "Interrupts Activate!!!\r\n";
-char *current_character = 0x00;
-
-//intput var
-char inputArray[64];
-char currentChar = ' ';   
-int arrCounter = 0x00;  
+} SerialPort;  
  
 // instantiate the serial port parameters
 //   note: the complexity is hidden in the c file
@@ -34,7 +38,7 @@ void SerialInitialiseBasic(SerialPort *serial_port) {
   *(serial_port->BaudLow)=156;
   //input set up *(serial_port->ControlRegister2) = SCI1CR2_RE_MASK|SCI1CR2_TE_MASK|SCI1CR2_RIE_MASK;    
   //output set up
-  *(serial_port->ControlRegister2) = SCI1CR2_RE_MASK|SCI1CR2_TE_MASK|SCI1CR2_TCIE_MASK;     
+  *(serial_port->ControlRegister2) = SCI1CR2_RE_MASK|SCI1CR2_TE_MASK|SCI1CR2_TCIE_MASK|SCI1CR2_RIE_MASK;     
   *(serial_port->ControlRegister1) = 0x00;
 }   
 //struct string_Buufer  
@@ -48,6 +52,17 @@ void SerialOutputChar(char data, SerialPort *serial_port) {
   
   *(serial_port->DataRegister) = data; 
 }
+ 
+void SerialInputChar(SerialPort *serial_port){
+  currentChar = *(serial_port->DataRegister);   
+}
+
+void ClearArr(){
+        //reset the arr counter
+       int arrCounter = 0;    
+       //clear array
+       memset(inputArray, 0, ARRSAPCE);       
+}
 
 interrupt VectorNumber_Vsci1 void SerialInterruptHandler(){
   //output
@@ -55,11 +70,16 @@ interrupt VectorNumber_Vsci1 void SerialInterruptHandler(){
     SerialOutputChar(*(current_character++), &SCI1);
   }
   //input
-  /*else if (!(*(SCI1.StatusRegister) & SCI1SR1_TDRE_MASK)){ 
-       //SerialInputChar(&SCI1); 
-       currentChar = inputArray[arrCounter];
-       arrCounter++; 
-  }*/ 
+  else if(*(SCI1.StatusRegister) & SCI1SR1_RDRF_MASK){ 
+       SerialInputChar(&SCI1);
+       if(currentChar == 13){
+         //end of input 
+        //ClearArr();
+        //main(); 
+       } 
+       inputArray[arrCounter] = currentChar;
+       arrCounter++;  
+  } 
   else if (*current_character == 0x00){
     
     // string is finished, stop the transmit interrupt from firing
@@ -67,24 +87,23 @@ interrupt VectorNumber_Vsci1 void SerialInterruptHandler(){
   }
 }         
 
-//interrupt 21 void GetIn();  
-
 void main(void){  
  SerialInitialiseBasic(&SCI1);  
 
   EnableInterrupts
   //output                                              
-    current_character = &string[0];
+  current_character = &string[0];
     
     // enable the transmit mask 
-    *(SCI1.ControlRegister2) |= SCI1CR2_TCIE_MASK;
+  *(SCI1.ControlRegister2) |= SCI1CR2_TCIE_MASK;
     
     // interrupts are enabled, only send the first char then the interrupts will send the rest one at a time
-    SerialOutputChar(*(current_character++), &SCI1);
+  SerialOutputChar(*(current_character++), &SCI1);
     
-    while (*current_character != 0x00) {
+  while (*current_character != 0x00) {
       // waiting in here until the string has completed sending
-    }
+  }
+  
   while(1){}    
 
-}  
+}    
